97638d838a5a5bbfbd8d01ac8d59322f31a5a662
Antony Donovan <antony.donovan@generalassemb.ly>
2016-09-17 10:36:12 -0400

    Use Adaptor `host` in createRecord kludge
    For nested resources

--- app/item/adapter.js
+++ app/item/adapter.js
@@ -2,9 +2,10 @@ import ApplicationAdapter from 'ga-wdi-boston.ember-resources/application/adapte
 
 export default ApplicationAdapter.extend({
   createRecord (store, type, record) {
+    let api = this.get('host');
     let serialized = this.serialize(record, { includeId: true });
     let parentId = serialized.list_id;
-    let url = `/lists/${parentId}/items`;
+    let url = `${api}/lists/${parentId}/items`;
     let data = { item: serialized };
 
     return this.ajax(url, 'POST', { data });
