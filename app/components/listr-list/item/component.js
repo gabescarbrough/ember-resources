16f39fb721f6792dd176a6014c7f35bf21377711
gabescarbrough <gabescarbrough@gmail.com>
2016-09-19 15:28:37 -0400

    Add delete item to ListR
    Can now delete items on list by clicking button

--- app/components/listr-list/item/component.js
+++ app/components/listr-list/item/component.js
@@ -8,5 +8,8 @@ export default Ember.Component.extend({
     toggleDone () {
       this.sendAction('toggleDone', this.get('item'));
     },
+    delete () {
+      this.sendAction('delete', this.get('item'));
+    },
   },
 });
--- app/list/route.js
+++ app/list/route.js
@@ -4,11 +4,15 @@ export default Ember.Route.extend({
   model (params) {
     return this.get('store').findRecord('list', params.list_id);
   },
-  
+
   actions: {
     toggleItemDone (item) {
       item.toggleProperty('done');
       item.save();
+    },
+
+    deleteItem (item) {
+      item.destroyRecord();
     },
   },
 });
